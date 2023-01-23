e4924e2e2c4783ac549006b5c884aa6eab98d83f
john-masters <mastersjohnr@gmail.com>
2023-01-23 15:23:31 +1100

    Added basic components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import '../App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Home'
+import User from './User'
+import Recipe from './Recipe'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/user' element={<User />} />
+      <Route path='/recipe' element={<Recipe />} />
     </Routes>
   )
 }
