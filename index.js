1e50b46371c31921ebda32dc9d5a61365178e7e4
kenny791 <3154018+kenny791@users.noreply.github.com>
2023-01-23 13:37:06 +1100

    added routes to return objects (#4)

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 import express from 'express'
 
+const users = [
+    {username: "user1" },
+    {username: "user2" },
+    {username: "user3" }
+]
+
+const recipes = [
+    {name: "recipe1", author: "user1"},
+    {name: "recipe2", author: "user2"},
+    {name: "recipe3", author: "user3"}
+]
+
 //create a new instance of express
 const app = express()
 //port number for the server to listen on
@@ -12,6 +24,10 @@ app.get("/", (request, response) => response.send("<h2>Hello world!</h2>"))
 
 //route to send test object
 app.get("/object", (request, response) => response.send({message: "Hello world!"}))
+
+app.get("/users", (request, response) => response.status(200).send(users))
+
+app.get("/recipes", (request, response) => response.status(200).send(recipes))
 
 
 
