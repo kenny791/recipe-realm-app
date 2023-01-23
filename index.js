fdcb84df21c620a3ac8bc0d6d9d2033a8ef62994
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2023-01-23 21:40:11 +1100

    add routes to post, and return single recipe

--- index.js
+++ index.js
@@ -29,8 +29,22 @@ app.get("/users", (request, response) => response.status(200).send(users))
 
 app.get("/recipes", (request, response) => response.status(200).send(recipes))
 
+app.get("/recipes/:id", (request, response) => {
+    const recipe = recipes[request.params.id]
+    if (recipe) {
+        response.send(recipe)
+    } else {
+        response.status(404).send({message: "Recipe not found"})
+    }
+})
+
 app.post("/users", (request, response) => {
-    response.send(request.body)
+    const { username } = request.body
+    //sanitise the data
+    const newUser = { username }
+    users.push(newUser)
+    response.status(201).send(newUser)
+
 })
 
 
