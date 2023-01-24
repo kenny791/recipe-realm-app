576a12f61451c17ca71548e81fd526d15bb10d11
Vickyyn <13328@coderacademy.edu.au>
2023-01-24 13:14:38 +1100

    added search bar and filter boxes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,62 @@ import React from 'react'
 export default () => {
   return (
     <>
-        <h1>Search</h1>
+			
+			{/* Search bar */}
+			{/* mb is margin bottom; w is width */}
+      <div class="form-floating mb-3 w-75">
+        <input class="form-control" id="floatingInput" />
+        <label for="floatingInput">Search</label>
+      </div>
+			{/* Filter dropdowns */}
+			{/* Still need to figure out how to make all boxes same size  */}
+			<div class="d-flex flex-wrap align-items-center p-2 w-75">
+				<div class="p-2 flex-fill">
+					<div class="form-floating">
+						<select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+							<option selected>Cuisine</option>
+							<option value="1">One</option>
+							<option value="2">Two</option>
+							<option value="3">Mediterranean</option>
+						</select>
+						<label for="floatingSelect">Filter</label>
+					</div>
+				</div>
+				<div class="p-2 flex-fill">
+					<div class="form-floating">
+						<select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+							<option selected>Dietary</option>
+							<option value="1">One</option>
+							<option value="2">Two</option>
+							<option value="3">Gluten Free</option>
+						</select>
+						<label for="floatingSelect">Filter</label>
+					</div>
+				</div>
+				<div class="p-2 flex-fill">
+					<div class="form-floating">
+						<select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+							<option selected>Difficulty</option>
+							<option value="1">One</option>
+							<option value="2">Two</option>
+							<option value="3">Three</option>
+						</select>
+						<label for="floatingSelect">Filter</label>
+					</div>
+				</div>
+				<div class="p-2 flex-fill">
+				<div class="form-floating">
+					<select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+						<option selected>Other</option>
+						<option value="1">One</option>
+						<option value="2">Two</option>
+						<option value="3">Three</option>
+					</select>
+					<label for="floatingSelect">Filter</label>
+				</div>
+			</div>
+			</div>
+			
     </>
   )
 }
