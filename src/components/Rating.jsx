3916c5651a0b2ee1021d3ff40e959e8be20d8d17
john-masters <mastersjohnr@gmail.com>
2023-01-26 12:01:08 +1100

    Rating component completed, recipe page modularised

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default () => {
+export default (props) => {
+  const [rating, setRating] = useState(0)
+  console.log(props)
+
+  const handleClick = (event) => {
+    const list = event.target.parentNode.children
+    const index = Array.from(list).indexOf(event.target)
+    setRating(index + 1)
+  }
+
+const stars = []
+  for (let i = 0; i < 5; i++) {
+    if (i < rating) {
+      stars.push(<span key={i} onClick={handleClick}>★️️️️️</span>)
+    } else {
+      stars.push(<span key={i} onClick={handleClick}>☆</span>)
+    }
+  }
+
   return (
     <div className='container'>
-            <p>☆☆☆★★️️</p>
+      {stars}
     </div>
   )
 }
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,43 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import Rating from './Rating'
+import RecipeMenu from './RecipeMenu'
 
 export default () => {
   return (
     <div className='container'>
-      <div className='row'>
-        <div className='col'>
-          <img src='https://placekitten.com/g/400/300' />
-        </div>
-        <div className='col'>
-          <div className='row'>
-            <h1>Recipe Name</h1>
-          </div>
-          <div className='row'>
-            <div className='col'>
-              <h5>Author Name</h5>
-            </div>
-            <div className='col'>
-              {/* Anchor tag to link to comment section */}
-              <h5><Link to='#comment-anchor'>Comments</Link></h5>
-            </div>
-          </div>
-          <div className='row'>
-            <div className='col'>
-              <div className='row'>
-                <h2>Rating</h2>
-              </div>
-              <div className='row'>
-                <Rating />
-              </div>
-            </div>
-            <div className='col'>
-              {/* Save button */}
-              <button type="button" className="btn btn-danger" data-bs-toggle="button" autocomplete="off">Save</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <RecipeMenu />
       <div className='container'>
         <h3>Description</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore at quasi modi in deserunt quam, vero corrupti suscipit laborum quos hic aperiam sunt explicabo, consequatur aliquid error quibusdam itaque ducimus.</p>
