b74102d188a192349a4575f68517bcee49f0f237
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2023-01-27 20:48:05 +1100

    mod, navbar collapse toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default () => {
-  return (
-    <nav className="navbar fixed-top navbar-expand-lg bg-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Recipe Realm</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link" to="/search">
-                  Search
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/recipe">
-                  Recipes
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/user">
-                  Profile
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                 Log-in 
-                </Link>
-              </li>
-            </ul>
-        </div>
-      </div>
-    </nav>
-  )
+	const [isOpen, setIsOpen] = useState(false)
+	const handleLinkClick = () => setIsOpen(false)
+	return (
+		<nav className="navbar  fixed-top  navbar-expand-lg navbar-light bg-light">
+			<div className="container-fluid">
+				<Link className="navbar-brand" to="/">Recipe Realm</Link>
+				<button className="navbar-toggler" type="button" onClick={() => setIsOpen(!isOpen)}>
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`}>
+					<ul className="navbar-nav mb-2 mb-lg-0">
+						<li className="nav-item">
+							<Link className="nav-link" onClick={handleLinkClick} to="/search" >Search</Link>
+						</li>
+						<li className="nav-item" >
+							<Link className="nav-link" onClick={handleLinkClick} to="/recipe">Recipes</Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link" onClick={handleLinkClick} to="/user">Profile</Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link" onClick={handleLinkClick} to="/">Log-in</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	)
 }
