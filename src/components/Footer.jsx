126e5fed667182916793590cc0a6338dcee621e8
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2023-01-27 19:55:09 +1100

    added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <footer className="text-center text-muted">
+            ©2023 Recipe Realm
+        </footer>
+    </>
+    )
+}
+
+export default Footer
