fa81278ec30cdb22b09a796a2f1e8f67fc2e1f09
john-masters <mastersjohnr@gmail.com>
2023-01-27 16:21:36 +1100

    Updated comment section to work with useState

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default () => {
+export default ({addComment}) => {
+
+  const [comment, setComment] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    addComment(comment)
+    setComment('')
+  }
   return (
     <div className='container'>
-      <div className='row justify-content-md-center'>
-        <div className='col-12'>
-      <h3 id='comment-anchor'>Comment</h3>
+      <form onSubmit={handleSubmit}>
+        <div className='row justify-content-md-center'>
+          <div className='col-12'>
+            <h3 id='comment-anchor'>Comment</h3>
+          </div>
         </div>
-      </div>
-      <div className='row justify-content-md-center'>
-        <div className='col-12'>
-          <input type="text" className="form-control" id="floatingInput" />
+        <div className='row justify-content-md-center'>
+          <div className='col-12'>
+            <input type="text" className="form-control" value={comment} onChange={(event) => setComment(event.target.value)} id="floatingInput" />
+          </div>
         </div>
-      </div>
-      <div className='row justify-content-md-center'>
-        <div className='col-12'>
-          <button type="button" className="btn btn-secondary" data-bs-toggle="button" autoComplete="off">Publish</button>
+        <div className='row justify-content-md-center'>
+          <div className='col-12'>
+            <button type="submit" className="btn btn-secondary" data-bs-toggle="button" autoComplete="off">Publish</button>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   )
 }
--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Comment from './Comment'
 import CommentForm from './CommentForm'
 
 export default () => {
+  const [comments, setComments] = useState([])
+
+  const addComment = (newComment) => {
+    // Comment is added with 'User' placeholder name
+    setComments([...comments, { name: 'User', comment: newComment }])
+  }
+
   return (
     <div className='container'>
-      <CommentForm />
-      <Comment />
+      <CommentForm addComment={addComment} />
+      <Comment comments={comments} />
     </div>
   )
 }
