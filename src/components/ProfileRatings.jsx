15ba6efceb9e1e07b1cd3e6c561ddb5e57f18e33
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2023-02-01 00:47:09 +1100

    added user ratings to profile

---
+++ src/components/ProfileRatings.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default ({loggedInUser, setLoggedInUser, recipeList}) => {
+
+
+    // adds the user's rating and comment to the recipe object
+    for (let i = 0; i < recipeList.length; i++) {
+        let rating = recipeList[i].rating_list.find((rating) => rating.username.username === loggedInUser.username)
+        recipeList[i].rating = rating ? rating.rating : null
+    }
+    //loop through object if ratings is not null, add to ratings object with recipe name and id
+    const ratings = Object.values(recipeList).map(recipe => {
+        if (recipe.rating !== null) {
+            return (
+                <div className="container text-center">
+                    <Link to={`/recipe/${recipe.id}`}>
+                    <h3>{recipe.name}</h3>
+                    <p>{recipe.rating}</p>
+                    </Link>
+                </div>
+            )
+        }
+    })
+
+  return (
+    <div className="container text-center">
+      <h2>Ratings</h2>
+      <div class="container text-center">
+      {ratings}
+      </div>
+    </div>
+  )
+}
