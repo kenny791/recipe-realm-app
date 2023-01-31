ae92f6de719265437e3cc07b9c99263221ca62b3
Vickyyn <13328@coderacademy.edu.au>
2023-02-01 10:52:42 +1100

    added state to fields

--- src/components/NewRecipe.jsx
+++ src/components/NewRecipe.jsx
@@ -2,18 +2,29 @@ import React, { useState } from "react"
 
 
 const NewRecipe = ({ loggedInUser, setRecipeList }) => {
-//   const { category } = useParams()
-    const [entry, setEntry] = useState('')
+
+    const initialEntry = {
+        recipeName: '',
+        recipeDescription: '',
+        recipeTags: '',
+        recipeImage: '',
+        recipeIngredients: '',
+        recipeMethod: ''
+    }
+    const [entry, setEntry] = useState(initialEntry)
+    const { recipeName, recipeDescription, recipeTags, recipeImage, recipeIngredients, recipeMethod } = entry
 
     // Styling for divs for input
     const styled = {margin: '15px'}
 
-    // function updateEntry(evt) {
-    //     setEntry({
-    //         ...entry,
-    //         evt.target.name: evt.target.value
-    //     })
-    // }
+    function updateEntry(evt) {
+        console.log(evt.target)
+        setEntry({
+            ...entry,
+            [evt.target.id]: evt.target.value
+        })
+        console.log(entry)
+    }
 
     function submit(evt) {
         evt.preventDefault()
@@ -27,35 +38,35 @@ const NewRecipe = ({ loggedInUser, setRecipeList }) => {
             <form className="m-3 mt-5 w-75" onSubmit={submit}>
                 <div className="form-group" style={styled}>
                     <label htmlFor="name">Recipe Name</label>
-                    <input type="text" className="form-control" id="recipeName" />
+                    <input type="text" className="form-control" id="recipeName" onChange={updateEntry} value={recipeName}/>
                 </div>
                 <div className="form-group" style={styled}>
                     <label htmlFor="description">Description</label>
-                    <textarea className="form-control" id="recipeDescription" />
+                    <textarea className="form-control" id="recipeDescription" onChange={updateEntry} value={recipeDescription}/>
                 </div>
                 <div className="form-group" style={styled}>
                     <label htmlFor="tags">Recipe Tags  
                         <span className="fw-light">  (separated by commas)</span>
                     </label>
-                    <input type="text" className="form-control" id="recipeTags" placeholder="e.g. Asian, soup, chicken" />
+                    <input type="text" className="form-control" id="recipeTags" placeholder="e.g. Asian, soup, chicken" onChange={updateEntry} value={recipeTags}/>
                 </div>
                 <div className="form-group" style={styled}>
                     <label htmlFor="image">Image
                         <span className="fw-light">  (URL format, default image applied if none entered)</span>
                     </label>
-                    <input type="text" className="form-control" id="recipeImage" />
+                    <input type="text" className="form-control" id="recipeImage" onChange={updateEntry} value={recipeImage}/>
                 </div>
                 <div className="form-group" style={styled}>
                     <label htmlFor="ingredients">Ingredients 
                         <span className="fw-light">  (separated by semi-colons)</span>
                     </label>
-                    <textarea className="form-control" id="recipeIngredients" placeholder="e.g. 500g chicken (diced); 2 carrots; 2 onions; 2 stalks celery" />
+                    <textarea className="form-control" id="recipeIngredients" placeholder="e.g. 500g chicken (diced); 2 carrots; 2 onions; 2 stalks celery" onChange={updateEntry} value={recipeIngredients}/>
                 </div>
                 <div className="form-group" style={styled}>
                     <label htmlFor="methods">Method 
                         <span className="fw-light">  (separated by semi-colons)</span>
                     </label>
-                    <textarea className="form-control" id="recipeMethod" placeholder="e.g. 1. Cut carrots, onions and celeries into big chunks; 2. Place chicken and vegetables in large pot and boil for 30 minutes; 3. Serve with parsley" />
+                    <textarea className="form-control" id="recipeMethod" placeholder="e.g. 1. Cut carrots, onions and celeries into big chunks; 2. Place chicken and vegetables in large pot and boil for 30 minutes; 3. Serve with parsley" onChange={updateEntry} value={recipeMethod}/>
                 </div>
                 <input type="submit" className="btn btn-primary btn-lg mx-3" value="Submit"/>
             </form>
