bfa5b0afbe96c81321e7f0606310e85b566beda4
Vickyyn <13328@coderacademy.edu.au>
2023-01-31 16:06:39 +1100

    submit button working

--- src/components/NewRecipe.jsx
+++ src/components/NewRecipe.jsx
@@ -3,21 +3,29 @@ import React, { useState } from "react"
 
 const NewRecipe = ({ loggedInUser, setRecipeList }) => {
 //   const { category } = useParams()
-//   const [entry, setEntry] = useState('')
+    const [entry, setEntry] = useState('')
 
-//   function submit(evt) {
-//     evt.preventDefault()
-//     addEntry(category, entry)
-//   }
-
+    // Styling for divs for input
     const styled = {margin: '15px'}
+
+    // function updateEntry(evt) {
+    //     setEntry({
+    //         ...entry,
+    //         evt.target.name: evt.target.value
+    //     })
+    // }
+
+    function submit(evt) {
+        evt.preventDefault()
+        console.log(evt.target)
+    }
 
   return (
     <>
 		<div className="h-100 d-flex flex-column align-items-center justify-content-center m-5">
             <h1>Submit a new recipe</h1>
-            <form className="m-3 mt-5 w-75">
-                <div style={styled}>
+            <form className="m-3 mt-5 w-75" onSubmit={submit}>
+                <div className="form-group" style={styled}>
                     <label htmlFor="name">Recipe Name</label>
                     <input type="text" className="form-control" id="recipeName" />
                 </div>
@@ -25,13 +33,13 @@ const NewRecipe = ({ loggedInUser, setRecipeList }) => {
                     <label htmlFor="description">Description</label>
                     <textarea className="form-control" id="recipeDescription" />
                 </div>
-                <div style={styled}>
+                <div className="form-group" style={styled}>
                     <label htmlFor="tags">Recipe Tags  
                         <span className="fw-light">  (separated by commas)</span>
                     </label>
                     <input type="text" className="form-control" id="recipeTags" placeholder="e.g. Asian, soup, chicken" />
                 </div>
-                <div style={styled}>
+                <div className="form-group" style={styled}>
                     <label htmlFor="image">Image
                         <span className="fw-light">  (URL format, default image applied if none entered)</span>
                     </label>
@@ -49,8 +57,8 @@ const NewRecipe = ({ loggedInUser, setRecipeList }) => {
                     </label>
                     <textarea className="form-control" id="recipeMethod" placeholder="e.g. 1. Cut carrots, onions and celeries into big chunks; 2. Place chicken and vegetables in large pot and boil for 30 minutes; 3. Serve with parsley" />
                 </div>
+                <input type="submit" className="btn btn-primary btn-lg mx-3" value="Submit"/>
             </form>
-            <button type="submit" className="btn btn-primary btn-lg">Submit</button>
         </div>
         
     </>
