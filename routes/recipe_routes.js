df7c96020dd7be3c0982a2c5d6596e4560a39e00
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2023-02-01 11:59:15 +1100

    added delete comment route

--- routes/recipe_routes.js
+++ routes/recipe_routes.js
@@ -170,4 +170,31 @@ router.delete("/recipes/:id", async (request, response) => {
     }
 })
 
+
+//delete comment by recipe id and comment id
+router.delete("/recipes/:recipeId/comments/:commentId", async (request, response) => {
+    try {
+        const recipe = await RecipeModel.findById(request.params.recipeId)
+        if (recipe) {
+            const commentIndex = recipe.comments.findIndex(
+            comment => comment._id.toString() === request.params.commentId
+            )
+            if (commentIndex !== -1) {
+                recipe.comments.splice(commentIndex, 1)
+                await recipe.save()
+                response.send({
+                message: "Comment deleted",
+                comments: recipe.comments
+                })
+            } else {
+                response.status(404).send({ message: "Comment not found" })
+            }
+        } else {
+            response.status(404).send({ message: "Recipe not found" })
+        }
+    } catch (err) {
+        response.status(500).send({ error: err.message })
+    }
+})
+
 export default router
