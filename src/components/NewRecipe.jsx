5139b7078fa0ce887a79927b3448c66c6dc9e488
Vickyyn <13328@coderacademy.edu.au>
2023-02-01 11:17:21 +1100

    added required fields

--- src/components/NewRecipe.jsx
+++ src/components/NewRecipe.jsx
@@ -26,7 +26,7 @@ const NewRecipe = ({ loggedInUser, setRecipeList }) => {
 
     function submit(evt) {
         evt.preventDefault()
-        alert(`Successfully submitted! ${{entry}}`)
+        // alert(`Successfully submitted! ${{entry}}`)
         console.log(entry)
     }
 
@@ -37,17 +37,17 @@ const NewRecipe = ({ loggedInUser, setRecipeList }) => {
             <form className="m-3 mt-5 w-75" onSubmit={submit}>
                 <div className="form-group" style={styled}>
                     <label htmlFor="name">Recipe Name</label>
-                    <input type="text" className="form-control" id="recipeName" onChange={updateEntry} value={recipeName}/>
+                    <input type="text" className="form-control" id="recipeName" onChange={updateEntry} value={recipeName} required/>
                 </div>
                 <div className="form-group" style={styled}>
                     <label htmlFor="description">Description</label>
-                    <textarea className="form-control" id="recipeDescription" onChange={updateEntry} value={recipeDescription}/>
+                    <textarea className="form-control" id="recipeDescription" onChange={updateEntry} value={recipeDescription} required/>
                 </div>
                 <div className="form-group" style={styled}>
                     <label htmlFor="tags">Recipe Tags  
                         <span className="fw-light">  (separated by commas)</span>
                     </label>
-                    <input type="text" className="form-control" id="recipeTags" placeholder="e.g. Asian, soup, chicken" onChange={updateEntry} value={recipeTags}/>
+                    <input type="text" className="form-control" id="recipeTags" placeholder="e.g. Asian, soup, chicken" onChange={updateEntry} value={recipeTags} required/>
                 </div>
                 <div className="form-group" style={styled}>
                     <label htmlFor="image">Image
@@ -59,13 +59,13 @@ const NewRecipe = ({ loggedInUser, setRecipeList }) => {
                     <label htmlFor="ingredients">Ingredients 
                         <span className="fw-light">  (separated by semi-colons)</span>
                     </label>
-                    <textarea className="form-control" id="recipeIngredients" placeholder="e.g. 500g chicken (diced); 2 carrots; 2 onions; 2 stalks celery" onChange={updateEntry} value={recipeIngredients}/>
+                    <textarea className="form-control" id="recipeIngredients" placeholder="e.g. 500g chicken (diced); 2 carrots; 2 onions; 2 stalks celery" required onChange={updateEntry} value={recipeIngredients}/>
                 </div>
                 <div className="form-group" style={styled}>
                     <label htmlFor="methods">Method 
                         <span className="fw-light">  (separated by semi-colons)</span>
                     </label>
-                    <textarea className="form-control" id="recipeMethod" placeholder="e.g. 1. Cut carrots, onions and celeries into big chunks; 2. Place chicken and vegetables in large pot and boil for 30 minutes; 3. Serve with parsley" onChange={updateEntry} value={recipeMethod}/>
+                    <textarea className="form-control" id="recipeMethod" placeholder="e.g. 1. Cut carrots, onions and celeries into big chunks; 2. Place chicken and vegetables in large pot and boil for 30 minutes; 3. Serve with parsley" required onChange={updateEntry} value={recipeMethod}/>
                 </div>
                 <input type="submit" className="btn btn-primary btn-lg mx-3" value="Submit"/>
             </form>
