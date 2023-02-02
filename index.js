73d926e960d05b6f798874ea7530242f6cf1bd25
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2023-02-02 17:08:15 +1100

    moved index.js files to app.js

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
-import express, { response } from 'express'
-import { UserModel, RecipeModel } from './db.js'
-import recipeRoutes from './routes/recipe_routes.js'
-import userRoutes from './routes/user_routes.js'
-import authRoutes from './routes/auth_routes.js'
-import cors from 'cors'
-
-
-//create a new instance of express
-const app = express()
-
-
-//json middleware
-app.use(express.json())
-//cors middleware
-app.use(cors())
-
-//routers for the different routes
-app.use(recipeRoutes)
-app.use(authRoutes)
-app.use(userRoutes)
-
+import app from './app.js'
 
 
 //port number for the server to listen on
